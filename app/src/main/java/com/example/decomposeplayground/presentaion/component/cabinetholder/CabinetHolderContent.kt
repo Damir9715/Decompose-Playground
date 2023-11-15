10dd70b4856620793003205270e08b13e8a303a4
asanbayev <asanbayev@kolesa.kz>
2023-11-15 23:59:30 +0600

    убрал анимацию переходов

--- app/src/main/java/com/example/decomposeplayground/presentaion/component/cabinetholder/CabinetHolderContent.kt
+++ app/src/main/java/com/example/decomposeplayground/presentaion/component/cabinetholder/CabinetHolderContent.kt
@@ -5,8 +5,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import com.arkivanov.decompose.extensions.compose.jetpack.stack.Children
-import com.arkivanov.decompose.extensions.compose.jetpack.stack.animation.fade
-import com.arkivanov.decompose.extensions.compose.jetpack.stack.animation.stackAnimation
 import com.arkivanov.decompose.extensions.compose.jetpack.subscribeAsState
 import com.example.decomposeplayground.presentaion.component.cabinet.CabinetContent
 import com.example.decomposeplayground.presentaion.component.settings.SettingsContent
@@ -21,7 +19,6 @@ fun CabinetHolderContent(
     Children(
             stack = childStack,
             modifier = modifier,
-            animation = stackAnimation(fade()),
     ) {
         when (val child = it.instance) {
             is CabinetHolderComponent.Child.CabinetChild -> CabinetContent(
